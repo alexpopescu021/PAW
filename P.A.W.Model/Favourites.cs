4a44218c5a692efcdd4f46bdc44cdaabd4a37296
Popescu Alexandru <alexpopescu021@gmail.com>
2021-04-27 21:42:01 +0300

    Create Favourites.cs

---
+++ P.A.W.Model/Favourites.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAW.Model
+{
+    public class Favourites : DataEntity
+    {
+
+        public string userId { get; set; }
+
+        public List<Song> favourites { get; set; }
+    }
+}
